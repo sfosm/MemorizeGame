a4417dd583297ac15ece38cfc184718884d07fc7
Adrian Szewczak <aszewczak@outlook.com>
2022-05-15 21:06:53 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Adrian Szewczak on 15/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
